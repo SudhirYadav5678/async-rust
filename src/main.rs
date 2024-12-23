95640365088539f1f30c224aadd80f6e47531a04
SudhirYadav <sudhirkyadav51@gmail.com>
2024-12-23 13:07:43 +0000

    async await

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use error_chain::error_chain;
+
+
+error_chain! {
+    foreign_links{
+        IO(std::io::Error);
+        HttpRequest(reqwest::Error)
+    }
+}
+
+#[tokio::main]
+async fn main()->Result<()> {
     println!("Hello, world!");
+    Let res = reqwest::get("http://httpbin.org/get");
+    println!("Status:{}",res.Status());
+    
+    Let body =res.text().await?;
+    println!("Body",body);
+    Ok({})
 }
